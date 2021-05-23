5700f23a28eb229283c0250312fa84ca25a58e0c
Cyril Ethan de Guzman <cyril_deguzman@dlsu.edu.ph>
2021-05-24 06:35:29 +0800

    hooked up profile.js and profile.hbs and added a temporary click function

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
     // TODO: howl and add to database then reload body.
     $('#submit-howl').click(function () {
-        var howl = $("#post-howl");
         alert('!in progress');
     });
 
