719956e4695f1eeda046c672372b0646d8f369cd
Lemsic <louislemsic@gmail.com>
2021-05-23 11:33:47 +0800

    Homepage and Signup

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+    
+    let stars = document.getElementById('stars');
+    let moon = document.getElementById('moon');
+    let mountains_behind = document.getElementById('mountains_behind');
+    let text = document.getElementById('text');
+    let btn = document.getElementById('main_btn');
+    let mountains_front = document.getElementById('mountains_front');
+    let header = document.querySelector('header');
+
+    window.addEventListener('scroll', function(){
+        let value = window.scrollY;
+        stars.style.left = value * 0.75 + 'px';
+        moon.style.top = value * 1.05 + 'px';
+        mountains_behind.style.top = value * 0.5 + 'px';
+        mountains_front.style.top = value * 0 + 'px';
+        header.style.top = value * 0.5 + 'px';
+    });
 
     /**
      * Sign Up Button.
