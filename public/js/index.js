82af2c57112d25f71466d6f7d1f330888ba11698
Lemsic <louislemsic@gmail.com>
2021-05-24 16:22:17 +0800

    Homepage update and Login v2

--- public/js/index.js
+++ public/js/index.js
@@ -16,4 +16,49 @@ $(document).ready(function () {
         mountains_front.style.top = value * 0 + 'px';
         header.style.top = value * 0.5 + 'px';
     });
+
+    const cursorSpan = document.querySelector(".cursor");
+    const typedTextSpan = document.getElementById("text");
+    const textArray = ["Hear.", "Howl.", "Hunt."];
+    const typingDelay = 200;
+    const erasingDelay = 100;
+    const newTextDelay = 2000;
+    let textArrayIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+        if (charIndex < textArray[textArrayIndex].length) {
+            if(!cursorSpan.classList.contains("typing")) 
+                cursorSpan.classList.add("typing");
+
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+        } 
+        else {
+            cursorSpan.classList.remove("typing");
+            setTimeout(erase, newTextDelay);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+        } 
+        else {
+            cursorSpan.classList.remove("typing");
+            textArrayIndex++;
+            if(textArrayIndex>=textArray.length) textArrayIndex=0;
+            setTimeout(type, typingDelay + 1100);
+        }
+    }
+
+    type();
+
+    document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+        if(textArray.length) setTimeout(type, newTextDelay + 250);
+    });
 })
